ca783ae7352716a3f4c54c9700c14b66ede07ada
Vhodnoylogin <kill.spama@gmail.com>
2022-09-08 20:23:02 +0300

    final

--- src/main/java/ru/rtech/internship/Task1.java
+++ src/main/java/ru/rtech/internship/Task1.java
@@ -1,8 +1,9 @@
 package ru.rtech.internship;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Task1 {
-
-
     public static String capitalizedWord(String word) {
         if (word == null || word.isEmpty()) return word;
         return word.substring(0, 1).toUpperCase()
@@ -16,6 +17,9 @@ public class Task1 {
      */
     public String capitalize(String str) {
 //        throw new UnsupportedOperationException("Удалите эту строку и напишите реализацию");
-        return str;
+        String DELIM = " ";
+        return Arrays.stream(str.split(DELIM))
+                .map(Task1::capitalizedWord)
+                .collect(Collectors.joining(DELIM));
     }
 }
