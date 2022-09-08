76535e5075c32a661aa65180b65cb37e77eba7ac
Vhodnoylogin <kill.spama@gmail.com>
2022-09-08 20:18:03 +0300

    nword static fix

--- src/main/java/ru/rtech/internship/Task1.java
+++ src/main/java/ru/rtech/internship/Task1.java
@@ -3,27 +3,19 @@ package ru.rtech.internship;
 public class Task1 {
 
 
+    public static String capitalizedWord(String word) {
+        if (word == null || word.isEmpty()) return word;
+        return word.substring(0, 1).toUpperCase()
+                + word.substring(1).toLowerCase();
+    }
+
     /**
      * Напишите реализацию метода, который делает первую букву каждого слова большой остальные маленькими
      * (гляньте в тесты если не понятно описание выше!)
      * =)
      */
     public String capitalize(String str) {
-        throw new UnsupportedOperationException("Удалите эту строку и напишите реализацию");
-    }
-
-    public static class Nword {
-        protected String headWord = "";
-        protected String tailWord = "";
-
-        public Nword(String word) {
-            if (word == null || word.isEmpty()) return;
-            this.headWord = word.substring(0, 1);
-            this.tailWord = word.substring(1);
-        }
-
-        public String getCapitalizedWord() {
-            return this.headWord.toUpperCase() + this.tailWord.toLowerCase();
-        }
+//        throw new UnsupportedOperationException("Удалите эту строку и напишите реализацию");
+        return str;
     }
 }
--- src/test/java/ru/rtech/internship/mytests/NWordTest.java
+++ src/test/java/ru/rtech/internship/mytests/NWordTest.java
@@ -6,10 +6,35 @@ import ru.rtech.internship.Task1;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class NWordTest {
-    protected Object ass;
+    protected static Object ass;
 
     @Test
     public void tezd_1() {
-        ass = assertThat(new Task1.Nword("vladimir").getCapitalizedWord().equals("Vladimir"));
+        ass = assertThat(Task1.capitalizedWord("vladimir").equals("Vladimir"));
+    }
+
+    @Test
+    public void tezd_2() {
+        ass = assertThat(Task1.capitalizedWord("vLADIMIr").equals("Vladimir"));
+    }
+
+    @Test
+    public void tezd_3() {
+        ass = assertThat(Task1.capitalizedWord("v").equals("V"));
+    }
+
+    @Test
+    public void tezd_4() {
+        ass = assertThat(Task1.capitalizedWord("").equals(""));
+    }
+
+    @Test
+    public void tezd_5() {
+        ass = assertThat(Task1.capitalizedWord("!!!!").equals("!!!!"));
+    }
+
+    @Test
+    public void tezd_6() {
+        ass = assertThat(Task1.capitalizedWord("!!!!fdgdgd").equals("!!!!fdgdgd"));
     }
 }
